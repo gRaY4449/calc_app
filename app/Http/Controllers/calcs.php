49bb0e6a4ea713932f199c48349c6590c88ad5f3
gRaY4449 <onlyforschoolclass378@gmail.com>
2022-10-09 17:27:21 +0900

    計算アプリ課題完了

---
+++ app/Http/Controllers/calcs.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class calcs extends Controller
+{
+    public function result($value1, $operator, $value2)
+    {
+
+        switch ($operator) {
+            case 'addition':
+                $result = $value1 + $value2;
+                break;
+            case 'substraction':
+                $result = $value1 - $value2;
+                break;
+            case 'multiplication':
+                $result = $value1 * $value2;
+                break;
+            case 'definition':
+                $result = $value1 / $value2;
+                break;
+        }
+        return view('message.result', ['result' => $result]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/calcs/{value1}/{operator}/{value2}', [App\Http\Controllers\calcs::class, 'result']);
